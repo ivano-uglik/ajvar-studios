6d7e4bfbb76c175a247c0d21f163d43fd8effdfe
ivano-uglik <ivano.uglik7@gmail.com>
2024-05-31 00:22:00 +0200

    edited zombie towers grid, and updated contact us page

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -2,21 +2,32 @@
 
 import { Unbounded } from "next/font/google";
 import Button from "../components/Button";
+import Link from "next/link";
 // import Liquid from "./Liquid";
 const unbounded = Unbounded({ subsets: ["latin"] });
 
 export default function ContactUs() {
   return (
     <div>
-      <div className="py-24 text-center">
+      <div className="pt-24 text-center">
         <h2
           className={`${unbounded.className} font-black uppercase text-4xl lg:text-5xl text-ajvar pb-8`}
         >
           Contact Us
         </h2>
-        <h3 className="lg:w-1/3 xl:w-1/4 mx-auto text-lg lg:text-xl">
-          Besides helping other companies and studios with game design
+        <h3 className="px-4 md:w-2/3 xl:w-1/3 mx-auto text-lg lg:text-xl pb-12">
+          {
+            " Got a brilliant idea or just want to chat? Drop us an email and let's press start on something amazing! "
+          }
         </h3>
+        <Link href={"mailto:ajvar.studio@gmail.com"}>
+          <button
+            className={`${unbounded.className} text-white bg-ajvar font-black rounded-full px-8 py-6 flex justify-between gap-4 mx-auto hover:scale-125 transition-all duration-300 ease-in-out text-xl mb-12`}
+          >
+            <h3 className="uppercase">Email Us</h3>
+            <span>@</span>
+          </button>
+        </Link>
       </div>
       <div
         className={`${unbounded.className} bg-ajvar py-24 text-center text-white text-3xl lg:text-5xl font-black`}
@@ -24,30 +35,33 @@ export default function ContactUs() {
         <h3 className="pb-4">Well, what are you waiting for?</h3>
         <h3>{"Contact us :)"}</h3>
         <div className="flex flex-wrap gap-4 justify-center lg:justify-around lg:gap-0 pt-8 lg:pt-24 content-container mx-auto">
-          <Button
-            text="LinkedIn"
-            bg="bg-white"
-            textColor="text-ajvar"
-            px="px-12"
-          />
-          <Button
-            text="Behance"
-            bg="bg-white"
-            textColor="text-ajvar"
-            px="px-12"
-          />
-          <Button
-            text="Instagram"
-            bg="bg-white"
-            textColor="text-ajvar"
-            px="px-12"
-          />
-          <Button
-            text="Email"
-            bg="bg-white"
-            textColor="text-ajvar"
-            px="px-12"
-          />
+          <Link
+            href={"https://www.linkedin.com/in/zoran-domuzin/"}
+            target="blank"
+          >
+            <Button
+              text="LinkedIn"
+              bg="bg-white"
+              textColor="text-ajvar"
+              px="px-12"
+            />
+          </Link>
+          <Link href={"https://www.behance.net/domuzin"} target="blank">
+            <Button
+              text="Behance"
+              bg="bg-white"
+              textColor="text-ajvar"
+              px="px-12"
+            />
+          </Link>
+          <Link href={"https://www.instagram.com/ajvarz"} target="blank">
+            <Button
+              text="Instagram"
+              bg="bg-white"
+              textColor="text-ajvar"
+              px="px-12"
+            />
+          </Link>
         </div>
       </div>
       {/* <Liquid /> */}
--- app/games/zombie-vs-towers/sections/ZombieTowersGrid.tsx
+++ app/games/zombie-vs-towers/sections/ZombieTowersGrid.tsx
@@ -7,18 +7,18 @@ import grid4 from "@/public/zombie-towers-assets/pic_zt_4.png";
 
 export default function Grid() {
   return (
-    <div className="grid grid-cols-2 grid-rows-3 mx-auto gap-x-2 gap-y-4 lg:gap-x-4 lg:gap-y-8 py-8 lg:py-16 px-4 lg:px-0 h-[80vh] lg:h-[100vh] lg:w-[40vw]">
+    <div className="grid grid-cols-2 grid-rows-3 mx-auto gap-x-2 gap-y-4 lg:gap-x-4 lg:gap-y-8 py-8 lg:py-16 px-4 xl:px-0 h-[80vh] lg:h-[200vh] content-container">
       <Image
         src={grid1}
         alt="OctoCurse gameplay"
         placeholder="blur"
-        className="row-span-2 rounded-xl lg:rounded-3xl h-full w-full object-cover"
+        className="row-span-2 rounded-xl lg:rounded-3xl h-full w-full object-cover object-top"
       />
       <Image
         src={grid2}
         alt="OctoCurse gameplay"
         placeholder="blur"
-        className="lrounded-xl g:rounded-3xl h-full w-full object-cover"
+        className="rounded-xl lg:rounded-3xl h-full w-full object-cover"
       />
       <Image
         src={grid3}
@@ -30,7 +30,7 @@ export default function Grid() {
         src={grid4}
         alt="OctoCurse gameplay"
         placeholder="blur"
-        className="lrounded-xl g:rounded-3xl h-full w-full object-cover"
+        className="rounded-xl lg:rounded-3xl h-full w-full object-cover"
       />
     </div>
   );
